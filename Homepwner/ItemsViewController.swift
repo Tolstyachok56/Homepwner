885bdcf5899f302740ebd5e55d9ec16a7220227e
Viktoriia Badisova <vbadisova@gmail.com>
2018-05-29 14:35:58 +0300

    create TableViewController as initial VC

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Виктория Бадисова on 29.05.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
