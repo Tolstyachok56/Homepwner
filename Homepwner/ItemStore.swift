3ba79684c7b010b7fd6341184cadbcd8a0f18334
Viktoriia Badisova <vbadisova@gmail.com>
2018-05-29 17:24:08 +0300

    set TableViewDataSource's methods

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -11,6 +11,12 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
