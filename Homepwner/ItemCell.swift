c9862e4b771b043f1c870f195f7f8b9e171869c8
Viktoriia Badisova <vbadisova@gmail.com>
2018-05-30 16:49:47 +0300

    create ItemCell class

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Виктория Бадисова on 30.05.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
