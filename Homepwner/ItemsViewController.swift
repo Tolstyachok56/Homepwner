2e96865afdd579575a5203efc79ef070b3f1b2ef
Viktoriia Badisova <vbadisova@gmail.com>
2018-05-30 12:18:48 +0300

    Gold Challenge: Customizing the Table

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -14,12 +14,30 @@ class ItemsViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setTableViewInsets()
+        setTableViewBackgroundImage()
+    }
+    
+    //MARK: - Configure UI
+    func setTableViewInsets() {
         let statusBarHeight = UIApplication.shared.statusBarFrame.height
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
     }
+    
+    func setTableViewBackgroundImage() {
+        let imageView = UIImageView(image: UIImage(named: "house"))
+        imageView.contentMode = .scaleAspectFill
+        tableView.backgroundView = imageView
+        
+        imageView.topAnchor.constraint(equalTo: tableView.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: tableView.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: tableView.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor).isActive = true
+    }
+    
+    //MARK: - UITableViewDataSource methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count + 1
@@ -32,11 +50,24 @@ class ItemsViewController: UITableViewController {
             let item = itemStore.allItems[indexPath.row]
             cell.textLabel?.text = item.name
             cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+            cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 20)
         } else {
             cell.textLabel?.text = "No more items!"
             cell.detailTextLabel?.text = ""
         }
+        cell.backgroundColor = .clear
         return cell
+    }
+    
+    //MARK: - UITableViewDelegate methods
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row < itemStore.allItems.count {
+            return 60
+        } else {
+            return 44
+        }
     }
     
 }
